307596554fbbc4c1a54083a853b27c9cde7383f4
Francisco Lopes <up202108796@up.pt>
2023-10-17 10:32:50 +0100

    correction

--- Proj1/utils.pl
+++ Proj1/utils.pl
@@ -6,16 +6,6 @@ letter(3, 'D').
 letter(4, 'E').
 letter(5, 'F').
 letter(6, 'G').
-letter(7, 'H').
-letter(8, 'I').
-letter(9, 'J').
-letter(10, 'K').
-letter(11, 'L').
-letter(12, 'M').
-letter(13, 'N').
-letter(14, 'O').
-letter(15, 'P').
-letter(16, 'Q').
 
 /**Winner*/
 announce(Winner) :-
